6faaf3ec0d62c4a4ddb122cb4e57baf42b126129
johanpoyet <jojopoy2003@gmail.com>
2025-03-19 09:38:45 +0100

    modif index

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
 import userRouter from './routes/userRoutes';
 import projectRouter from './routes/projectRoutes';
 import galleryRouter from './routes/galleryRoutes';
 import { connectDB } from './config/database';
 import { initializeUsers } from './config/initUsers';
+import { cors } from 'hono/cors'
 
-const app = new Hono()
+const app = new Hono();
 
+app.use(cors())
 
 await connectDB();
 await initializeUsers();
+
 app.route('/api/users', userRouter);
 app.route('/api/projects', projectRouter);
 app.route('/api/galleries', galleryRouter);
 
+const PORT = process.env.PORT || 3001;
 
-const port = 3000
-serve(app)
+serve({
+  fetch: app.fetch,
+  port: Number(PORT),
+});
+
+console.log(`Server running on http://localhost:${PORT}`);
